cacc83c4b8dc68318eef2e451c84ad2f5140223c
김광환 <louie1004@naver.com>
2026-06-16 15:16:27 +0900

    Initial commit

---
+++ Assets/Scripts/HelloWorldLifeTimeScope.cs
@@ -0,0 +1,11 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+public class HelloWorldLifeTimeScope : LifetimeScope
+{
+    protected override void Configure(IContainerBuilder builder)
+    {
+        builder.Register<GreetingService>(Lifetime.Singleton);
+        builder.RegisterEntryPoint<GreetingEntryPoint>();
+    }
+}
